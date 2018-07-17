9e6f1f257d1ebaea1c6c8c01eeb4552bba2582a4
Ricky Houghton <ricky@iamrobotics.com>
2018-07-17 12:44:24 -0400

    e

--- src/main/java/com/replaymod/replay/ReplayHandler.java
+++ src/main/java/com/replaymod/replay/ReplayHandler.java
@@ -412,6 +412,7 @@ public class ReplayHandler {
             if (diff > 0 && diff < 5000) { // Small difference and no time travel
                 replaySender.jumpToTime(targetTime);
             } else { // We either have to restart the replay or send a significant amount of packets
+				LogManager.getLogger().debug("RAH: doJump diff " + diff);
                 // Render our please-wait-screen
                 GuiScreen guiScreen = new GuiScreen() {
                     @Override
