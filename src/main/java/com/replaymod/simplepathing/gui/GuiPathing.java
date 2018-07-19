d93f9fc87f5b874dbb9b2d62b82649c0c78488f2
Ricky Houghton <ricky@iamrobotics.com>
2018-07-18 20:54:18 -0400

    d

--- src/main/java/com/replaymod/simplepathing/gui/GuiPathing.java
+++ src/main/java/com/replaymod/simplepathing/gui/GuiPathing.java
@@ -765,7 +765,7 @@ public class GuiPathing {
                     mod.setSelected(null, 0);
                 } else {
                     LOGGER.debug("No time keyframe found -> adding new keyframe");
-                    timeline.addTimeKeyframe(time, currentTimeStamp());
+                    timeline.addTimeKeyframe(time, currentTimeStamp);
                     mod.setSelected(path, time);
                 }
                 break;
