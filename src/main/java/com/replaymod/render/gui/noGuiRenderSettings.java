86bec79609fd43268a4d782b0baf722d387f5160
Ricky Houghton <ricky@iamrobotics.com>
2018-07-19 14:12:12 -0400

    g

--- src/main/java/com/replaymod/render/gui/noGuiRenderSettings.java
+++ src/main/java/com/replaymod/render/gui/noGuiRenderSettings.java
@@ -139,11 +139,10 @@ public class noGuiRenderSettings  {
     }
 
     private RenderSettings getDefaultRenderSettings() {
-	// RAh - the null was for serialized - we are going to file only
-        //return new RenderSettings(RenderSettings.RenderMethod.DEFAULT, RenderSettings.EncodingPreset.MP4_DEFAULT, 1920, 1080, 60, 10 << 20, null,
+		// RAh - the null was for serialized - we are going to file only - replaced null with generateOutputFile()
 		int width = 320; // RAH - made these variables to be more easily readable
 		int height = 240;
-		int frameRate = 15;
+		int frameRate = 5;
 		// N.B. When bitRate was set to 10, a 1920x1080 at 60 fps resulted in a final bitRate in the file of 19,930 kbps - that is a crazy amount of data
 		int bitRate = 10; // Do know what this is, mbps, kbps - some other value? There is a shift happening - be careful
 		return new RenderSettings(RenderSettings.RenderMethod.DEFAULT, RenderSettings.EncodingPreset.MP4_DEFAULT, width, height, frameRate, bitRate << 20, generateOutputFile(),
