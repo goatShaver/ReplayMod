4659f4b91d4cf134c7cc5ad2617400c5db5eea88
Ricky Houghton <ricky@iamrobotics.com>
2018-07-30 11:49:03 -0400

    Another attempt to remove the doJump, this time we are sleeping longer than 100

--- src/main/java/com/replaymod/render/gui/noGuiRenderSettings.java
+++ src/main/java/com/replaymod/render/gui/noGuiRenderSettings.java
@@ -160,7 +160,7 @@ public class noGuiRenderSettings  {
 		// RAh - the null was for serialized - we are going to file only - replaced null with generateOutputFile()
 		int width = 320; // RAH - made these variables to be more easily readable
 		int height = 240;
-		int frameRate = 20;
+		int frameRate = 5;
 		// N.B. When bitRate was set to 10, a 1920x1080 at 60 fps resulted in a final bitRate in the file of 19,930 kbps - that is a crazy amount of data
 		int bitRate = 8; // was 10 - Do know what this is, mbps, kbps - some other value? There is a shift happening - be careful
 		return new RenderSettings(RenderSettings.RenderMethod.DEFAULT, RenderSettings.EncodingPreset.MP4_DEFAULT, width, height, frameRate, bitRate << 20, generateOutputFile(),
